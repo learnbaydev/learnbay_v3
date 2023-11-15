1fcedc97f6f1babaef721648959e51e898b33e3d
somnath khadanga <somnathkhadanga@somnaths-MacBook-Pro.local>
2023-11-15 13:57:52 +0530

    course optimize part1

--- src/components/CoursePage/FirstPart/FirstPart.js
+++ src/components/CoursePage/FirstPart/FirstPart.js
@@ -1,4 +1,3 @@
-import React from "react";
 import dynamic from "next/dynamic";
 import { DataScienceCourseData } from "../../../Data/AdvanceDataScienceCourse";
 import Navbar from "@/components/Global/Navbar/Navbar";
