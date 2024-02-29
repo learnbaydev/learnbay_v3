12a9e5d760db5941c5747b17657e802fe50524e6
ammar-tanwar <ammartanwar123@gmail.com>
2024-02-29 17:54:27 +0530

    new enroll page

--- src/components/EnrollPage/EnrollPagePopup.js
+++ src/components/EnrollPage/EnrollPagePopup.js
@@ -1,13 +1,26 @@
-// NewsletterPopup.js
-
 import React, { useState } from "react";
+import axios from "axios";
 import styles from "./EnrollPage.module.css";
 import { FaRegCheckCircle } from "react-icons/fa";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
-const EnrollPopup = ({ onClose }) => {
+const EnrollPopup = ({
+  onClose,
+  name,
+  email,
+  phone,
+  selectedProduct,
+  selectedCoupon,
+  totalPrice,
+  paymentType,
+}) => {
   const [selectedDate, setSelectedDate] = useState("");
   const [selectedTime, setSelectedTime] = useState("");
+  const [submittingForm, setSubmittingForm] = useState(false);
+  const [formSubmitted, setFormSubmitted] = useState(false);
+  const router = useRouter();
+
   const handleDateChange = (e) => {
     setSelectedDate(e.target.value);
   };
@@ -15,11 +28,45 @@ const EnrollPopup = ({ onClose }) => {
   const handleTimeChange = (e) => {
     setSelectedTime(e.target.value);
   };
+
+  const handleFormSubmit = () => {
+    setSubmittingForm(true);
+    const formData = {
+      date: selectedDate,
+      time: selectedTime,
+      name: name,
+      email: email,
+      phone: phone,
+      selectedProduct: selectedProduct.name,
+      selectedCoupon: selectedCoupon,
+      totalPrice: totalPrice,
+      paymentType: paymentType,
+      url: router.asPath,
+    };
+
+    axios
+    .post("https://getform.io/f/df003555-86c7-4ae5-a7f8-98c21dd9ad92", formData)
+    .then((response) => {
+      console.log("Form submitted successfully to external endpoint!");
+      return axios.post("/api/Database/emailConfirmation", formData);
+    })
+    .then((response) => {
+      console.log("Confirmation email sent successfully!");
+      setFormSubmitted(true);
+    })
+      .catch((error) => {
+        console.error("Error submitting form:", error);
+      })
+      .finally(() => {
+        setSubmittingForm(false);
+      });
+  };
+
   return (
     <div className={styles.overlay}>
       <div className={styles.popup}>
         <button className={styles.closeButton} onClick={onClose}>
-        Close[x]
+          Close[x]
         </button>
         <div className={styles.contPopup}>
           <div className={styles.mainPopup}>
@@ -49,7 +96,7 @@ const EnrollPopup = ({ onClose }) => {
             </div>
           </div>
           <div>
-          <Image
+            <Image
               src="https://d32and0ii3b8oy.cloudfront.net/web/s3_main/loansimg+(1).webp"
               alt="arrow"
               width={726}
@@ -58,7 +105,7 @@ const EnrollPopup = ({ onClose }) => {
               loading="lazy"
               quality={40}
             />
-            </div>
+          </div>
           <div>
             <form>
               <div className={styles.divFlexPopup}>
@@ -82,25 +129,48 @@ const EnrollPopup = ({ onClose }) => {
                     required={true}
                   />
                 </div>
+                <input
+                  type="hidden"
+                  id="url"
+                  name="url"
+                  value={router.asPath}
+                />
               </div>
-              <button
-              type="button"
-              onClick={() => {
-                if (isInstallmentSelected) {
-                  setShowPopup(true);
-                } else {
-                  makePayment();
-                }
-              }}
-              className={styles.button}
-              style={{width:"auto" , margin:"30px 0px 0px 0px"}}
-            >
-              Book an Appointment
-            </button>
+              {formSubmitted ? (
+                <div className={styles.successMessage}>
+                  Form submitted successfully! We will get back to you soon.
+                </div>
+              ) : (
+                <button
+                type="button"
+                onClick={() => {
+                  console.log("Checking if fields are empty...");
+                  if (!selectedTime || !selectedDate || submittingForm) {
+                    console.log("Fields are empty. Showing alert...");
+                    alert("Please fill in all the required fields.");
+                    return;
+                  }
+                  setSubmittingForm(true);
+                  console.log("Fields are filled. Proceeding with checkout...");
+                  handleFormSubmit();
+                }}
+                className={`${styles.button} ${
+                  (!selectedTime || !selectedDate) && !submittingForm ? styles.disabled : ""
+                }`}
+                disabled={!selectedTime || !selectedDate || submittingForm}
+                style={{
+                  width: "auto",
+                  margin: "30px 0px 0px 0px",
+                  opacity: submittingForm ? 0.5 : (!selectedTime || !selectedDate ? 0.5 : 1),
+                }}
+              >
+                {submittingForm ? "Submitting..." : "Book an Appointment"}
+              </button>
+              
+              )}
             </form>
           </div>
         </div>
-        {/* Add your newsletter signup form or content here */}
       </div>
     </div>
   );
