26f95004c12d2c4abac6d726ab736874908a4205
AmitAmbi <amit@learnbay.co>
2024-02-29 18:12:12 +0530

    pune dmd nigeria chnages xpertedge delete

---
+++ src/pages/data-science-courses.js
@@ -0,0 +1,74 @@
+import Head from "next/head";
+import { parseJSONData } from "@/Util/JsonConvertor";
+
+
+import Navbar from "@/components/Global/Navbar/Navbar";
+import DSCourseFirstPart from "@/components/CoursePage/FirstPart/DSCourseFirstPart";
+import DSCourseSecondPart from "@/components/CoursePage/FirstPart/DSCourseSecondPart"
+
+function Blockchain({ DataScienceCourseDataJson }) {
+  const DataScienceCourseData = parseJSONData(DataScienceCourseDataJson);
+
+  return (
+    <>
+      <Head>
+        <title>
+        Data Science Courses
+        </title>
+        <meta
+          name="description"
+          content="Are you searching for the Advanced Data Science and Artificial Intelligence Program With Certification? Learnbay is providing the best online Advanced Data Science and AI Program . Enroll now!"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta
+          name="keywords"
+          content="data science and ai course, advanced data science course, data science and ai, ai and data science course, artificial intelligence and data science course, advanced certification in data science and ai"
+        />
+        <link
+          rel="icon"
+          href="https://d32and0ii3b8oy.cloudfront.net/web/s3_main/cloud-computing/website-icon.webp"
+        />
+        <link
+          rel="canonical"
+          href="https://www.learnbay.co/datascience/advance-data-science-certification-courses"
+        />
+       
+      </Head>
+      <main>
+        <Navbar dataScienceCounselling={true} interstedInHide={true} />
+      
+        <DSCourseFirstPart
+          SecondSectionData={
+            DataScienceCourseData.DataScienceCourseData[0].secondSection
+          }
+          TestimonialData={
+            DataScienceCourseData.DataScienceCourseData[0].testimonial
+          }
+        />
+      </main>
+      <main>
+        <DSCourseSecondPart
+          masterSyllabusMobile={
+            DataScienceCourseData.DataScienceCourseData[0].masterSyllabusMobile
+          }
+          CertificateData={
+            DataScienceCourseData.DataScienceCourseData[0].Certificate
+          }
+          projectSection={
+            DataScienceCourseData.DataScienceCourseData[0].projectSection
+          }
+          FAQNewData={DataScienceCourseData.DataScienceCourseData[0].faq}
+        />
+      </main>
+    </>
+  );
+}
+export default Blockchain;
+export async function getStaticProps() {
+  const data = await import("../Data/DataScinceCourseOnly");
+  function getDataScienceCourseDataJSON(dataScienceCourseData) {
+    return JSON.stringify(dataScienceCourseData);
+  }
+  const DataScienceCourseDataJson = getDataScienceCourseDataJSON(data);
+  return { props: { DataScienceCourseDataJson } };
+}
