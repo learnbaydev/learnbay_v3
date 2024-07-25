d550db0926e8d5c26c6e3f0c161ad6f2aacc0e04
amit <amit@learnbay.co>
2024-07-25 17:07:31 +0530

    sitemap adjust

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,14 +1,14 @@
 // next-sitemap.config.js
 module.exports = {
-    siteUrl: 'https://www.learnbay.co/', // Replace with your domain for production
-    generateRobotsTxt: true, // (Optional) Generate robots.txt file
-    sitemapSize: 50000, // Max number of URLs per sitemap file
-    outDir: './public', // Ensure sitemap is output to the public directory
-    generateIndexSitemap: false, // Disable sitemap index generation
+    siteUrl: 'https://www.learnbay.co/', 
+    generateRobotsTxt: false, 
+    sitemapSize: 50000,
+    outDir: './public', 
+    generateIndexSitemap: false, 
     transform: async (config, url) => {
-      // Exclude URLs containing 'thank-you'
+   
       if (url.toLowerCase().includes('Thank-you')) {
-        return null; // This will exclude the URL from the sitemap
+        return null; 
       }
   
       const lastModified = new Date().toISOString();
