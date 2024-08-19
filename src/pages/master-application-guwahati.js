a14060e2233a23614a5ff97bd18c3ed3f6298a32
ammar-tanwar <ammartanwar123@gmail.com>
2024-08-19 12:29:54 +0530

    code merge with somnath and no index

--- src/pages/master-application-guwahati.js
+++ src/pages/master-application-guwahati.js
@@ -9,7 +9,6 @@ const masterApplication = () => {
         <title>
         Master - Application Guwahati Form
         </title>
-        <meta name="robots" content="index, nofollow" />
         <meta
           name="description"
           content=" Submit your information to LearnBay to unlock a world of learning opportunities. Join our platform today and start your path to success."
@@ -18,7 +17,7 @@ const masterApplication = () => {
           name="keywords"
           content="Data Science Course, Best data Science Course Training,   Data Science, Master Course Application "
         />
-                <meta name="robots" content="index, follow" />
+                <meta name="robots" content="noindex, nofollow" />
         <link
           rel="canonical"
           href="https://www.learnbay.co/master-application-guwahati"
