2e4d0327f68650c457f4f64c8ba66f685e5d251d
amit ambi <amit@learnbay.co>
2024-09-12 19:23:55 +0530

    new code with new popup and font change

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -6,44 +6,39 @@ import styles from "./Popup.module.css";
 
 const Popup = ({ message, onClose }) => {
   return (
-    <div className={styles.popupOverlay} onClick={onClose}>
+    <div className={styles.popupOverlay}>
       <div
         className={styles.popupContainer}
-        onClick={(e) => e.stopPropagation()}
+        onClick={(e) => e.stopPropagation()} // Prevents propagation to the overlay
       >
+     
         <span className={styles.close} onClick={onClose}>
           &times;
         </span>
-        <div className={styles.ImgDiv}>
-          <Image
-            src="https://d32and0ii3b8oy.cloudfront.net/web/V4/HomePage/iit_guwahati_p.webp"
-            width={550}
-            height={500}
-            alt="POPUP"
-            className={styles.popupImage}
-            priority
-          />
-        </div>
-        <div className={styles.popUpdown}>
-          <div className={styles.popupdivHead}>
-            <h4>Program Benefits:</h4>
-            <div className={styles.iconMain}>
-              <div className={styles.iconDiv}>
-                <BsRobot className={styles.icons} />
-                <p>Gen AI module with ChatGPT</p>
-              </div>
-              <div className={styles.iconDiv}>
-                {" "}
-                <IoCall className={styles.icons} />
-                <p>Unlimited Interview Calls</p>
-              </div>
-            </div>
-            <h3>Get 30% Scholarship</h3>
+
+        <div className={styles.mainDiv}>
+       
+          <div className={styles.whiteDiv}>
+            <h3>Get Scholarship upto</h3>
+            <span className={styles.OFF}>30% OFF</span>
           </div>
 
-          <div className={styles.btndiv}>
+          {/* Middle TagLine */}
+          <div className={styles.tagLine}>
+            <p>
+              on all Our <span>Course</span>
+            </p>
+          </div>
+
+          {/* Bottom Gradient Div */}
+          <div className={styles.gradientDiv}>
+            <p className={styles.offerText}>
+              Offer valid till: <span>14th Sep, 9:00 pm</span>
+            </p>
             <Link href="/submit-info" target="_blank">
-              <button className={styles.btn}>Apply For Scholarship Now!</button>
+              <div className={styles.buttonDiv}>
+                <button>Apply for Scholarship Now</button>
+              </div>
             </Link>
           </div>
         </div>
