518bc42bd845190872bc26b3e8ffe4a8a189bc11
Somsurya <somsurya@learnbay.co>
2025-03-30 20:59:42 +0530

    BFSI pdf changed , og image updated in some urls

--- src/pages/datascience/bangalore/data-science-course-training-in-bangalore.js
+++ src/pages/datascience/bangalore/data-science-course-training-in-bangalore.js
@@ -130,7 +130,7 @@ export default function Home() {
         />
         <meta
           property="og:image"
-          content="https://www.learnbay.co/_next/image?url=https%3A%2F%2Flearnbay-wb.s3.ap-south-1.amazonaws.com%2Fmain%2FLearnbay-Logo.webp&w=256&q=100"
+          content="https://d32and0ii3b8oy.cloudfront.net/web/s3_main/learnbayMain/learnbay-logo.png"
         />
         <meta property="og:image:height" content="100" />
         <meta property="og:image:width" content="256" />
@@ -151,7 +151,7 @@ export default function Home() {
         />
         <meta
           name="twitter:image"
-          content="https://www.learnbay.co/_next/image?url=https%3A%2F%2Flearnbay-wb.s3.ap-south-1.amazonaws.com%2Fmain%2FLearnbay-Logo.webp&w=256&q=100"
+          content="https://d32and0ii3b8oy.cloudfront.net/web/s3_main/learnbayMain/learnbay-logo.png"
         />
         <link
           rel="icon"
