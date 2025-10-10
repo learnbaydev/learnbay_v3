c6d339e9fd926c9a9ed48c98d74a52bfb58bdacf
sohel syed <syed.sohel@learnbay.co>
2025-10-10 18:14:01 +0530

    updated brocher links

--- src/components/homeNew/ProgramSection/programData.js
+++ src/components/homeNew/ProgramSection/programData.js
@@ -19,9 +19,9 @@ export const programCards = [
     logo: "https://d32and0ii3b8oy.cloudfront.net/web/s3_main/new-UI/ibm-microsoft+(1).webp",
     icon: "https://d32and0ii3b8oy.cloudfront.net/web/s3_main/Group+2408.webp",
     brochureUrl:
-      "https://brochureslearnbay.s3.ap-south-1.amazonaws.com/Data_Science_and_Generative_AI_Master_Certification_Program.pdf",
+      "https://brochureslearnbay.s3.ap-south-1.amazonaws.com/Data_Science_and_Generative_AI_Master_Certification%2BProgram.pdf",
     pdfUrl:
-      "https://brochureslearnbay.s3.ap-south-1.amazonaws.com/Data_Science_and_Generative_AI_Master_Certification_Program_download.pdf",
+      "https://brochureslearnbay.s3.ap-south-1.amazonaws.com/Data_Science_and_Generative_AI_Master_Certification_Program%2Bdownload.pdf",
     dataScience: true,
     dataScienceCounselling: true,
   },
