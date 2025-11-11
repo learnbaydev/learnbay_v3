fe10968fc44e0050b5874f48ba77410c59d72480
sohel <syed.sohel@learnbay.co>
2025-11-11 15:51:24 +0530

    corrected woolf link

--- src/Data/ExecutiveProgramDataGeneric.js
+++ src/Data/ExecutiveProgramDataGeneric.js
@@ -1664,8 +1664,8 @@ export const ExcutiveProgramAIDS = [
         ],
 
         link: false,
-        linkText: "https://legal.woolf.university/accreditation",
-        linkUrl: "https://legal.woolf.university/accreditation",
+        linkText: "https://woolf.university/colleges/learnbay",
+        linkUrl: "https://woolf.university/colleges/learnbay",
       },
       projectCertificate: {
         id: 1,
