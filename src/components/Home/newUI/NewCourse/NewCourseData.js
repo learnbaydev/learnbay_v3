6af01990242b614572535fc431a8769c24cdda6b
sohel <syed.sohel@learnbay.co>
2026-01-05 18:22:45 +0530

    devops course page link corrected

--- src/components/Home/newUI/NewCourse/NewCourseData.js
+++ src/components/Home/newUI/NewCourse/NewCourseData.js
@@ -1002,7 +1002,7 @@ export const courses = {
       description: "For Network Professionals",
       image:
         "https://d32and0ii3b8oy.cloudfront.net/web/s3_main/Course-home/NP.webp",
-      link: "/cloud&devops/cloud-devops-for-network-professionals",
+      link: "/cloud-devops-for-network-professionals",
       brochureLinks:
         "https://brochureslearnbay.s3.ap-south-1.amazonaws.com/Cloud-and-Devops-Engineering-For-Network-Professionalslink.pdf",
       brochurePdfs:
@@ -1016,7 +1016,7 @@ export const courses = {
       description: "For System Administrators",
       image:
         "https://d32and0ii3b8oy.cloudfront.net/web/s3_main/Course-home/SA.webp",
-      link: "/cloud&devops/cloud-and-devOps-for-system-administrators",
+      link: "/cloud-and-devOps-for-system-administrators",
       brochureLinks:
         "https://brochureslearnbay.s3.ap-south-1.amazonaws.com/Cloud-and-Devops-Engineering-For-System-Admin-Professionalslink.pdf",
       brochurePdfs:
@@ -1044,7 +1044,7 @@ export const courses = {
       description: "For IT Support & IT Operations",
       image:
         "https://d32and0ii3b8oy.cloudfront.net/web/s3_main/Course-home/ISIO.webp",
-      link: "/cloud&devops/cloud-and-devops-for-it-support-and-it-operations",
+      link: "cloud-and-devops-for-it-support-and-it-operations",
       brochureLinks:
         "https://brochureslearnbay.s3.ap-south-1.amazonaws.com/Cloud-and-Devops-Engineering-For-ITS-%26-ITOps-Professionalslink.pdf",
       brochurePdfs:
@@ -1058,7 +1058,7 @@ export const courses = {
       description: "For Database Administrators",
       image:
         "https://d32and0ii3b8oy.cloudfront.net/web/s3_main/Course-home/DA.webp",
-      link: "/cloud&devops/cloud-and-devops-for-database-administrators",
+      link: "cloud-and-devops-for-database-administrators",
       brochureLinks:
         "https://brochureslearnbay.s3.ap-south-1.amazonaws.com/Cloud-and-Devops-Engineering-For-DBA-Professionalslink.pdf",
       brochurePdfs:
