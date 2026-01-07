43692e30821916444649a57db32824919c10fb95
sohel <syed.sohel@learnbay.co>
2026-01-07 20:59:11 +0530

    cloud devops system admin pdf link

--- src/components/Home/newUI/NewCourse/NewCourseData.js
+++ src/components/Home/newUI/NewCourse/NewCourseData.js
@@ -1046,7 +1046,7 @@ export const courses = {
       brochureLinks:
         "https://brochureslearnbay.s3.ap-south-1.amazonaws.com/Cloud-and-Devops-Engineering-For-System-Admin-Professionalslink.pdf",
       brochurePdfs:
-        "https://brochureslearnbay.s3.ap-south-1.amazonaws.com/Cloud-and-Devops-Engineering-For-System-Admin-Professionals+.pdf",
+        "https://brochureslearnbay.s3.ap-south-1.amazonaws.com/2026/Cloud+and+Devops+Engineering+For+System+Admin+Professionals++(3)_compressed.pdf",
     },
 
     // {
@@ -1060,7 +1060,7 @@ export const courses = {
     //   brochureLinks:
     //     "https://brochureslearnbay.s3.ap-south-1.amazonaws.com/Cloud-and-Devops-Engineering-For-System-Admin-Professionalslink.pdf",
     //   brochurePdfs:
-    //     "https://brochureslearnbay.s3.ap-south-1.amazonaws.com/Cloud-and-Devops-Engineering-For-System-Admin-Professionals+.pdf",
+    //     "https://brochureslearnbay.s3.ap-south-1.amazonaws.com/2026/Cloud+and+Devops+Engineering+For+System+Admin+Professionals++(3)_compressed.pdf",
     // },
 
     {
--- src/pages/cloud-and-devOps-for-system-administrators.js
+++ src/pages/cloud-and-devOps-for-system-administrators.js
@@ -44,7 +44,7 @@ const SyllabusNew = dynamic(() =>
 const NewDSA = ({ DSADataJson }) => {
   const NewDSAData = parseJSONData(DSADataJson);
   const pdfUrl =
-    "https://brochureslearnbay.s3.ap-south-1.amazonaws.com/Cloud-and-Devops-Engineering-For-System-Admin-Professionals+.pdf";
+    "https://brochureslearnbay.s3.ap-south-1.amazonaws.com/2026/Cloud+and+Devops+Engineering+For+System+Admin+Professionals++(3)_compressed.pdf";
   return (
     <>
       <Head>
