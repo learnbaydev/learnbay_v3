d7eb45c9e87b75b9f04e3c84d04e5e5044612b7b
Sushma <sushmabendre@learnbay.co>
2026-02-17 11:07:00 +0530

    added usa

--- src/CityData/Usa/datascienceCourseTrainingInUsa.js
+++ src/CityData/Usa/datascienceCourseTrainingInUsa.js
@@ -168,7 +168,7 @@ export const DSUSACourseData = [
           {
             id: 3,
             ques: 'What is a Flexi Subscription in these advanced data science courses in USA?',
-            ans: 'Those who enroll for Data Science live classroom training are eligible for a Flexi Pass. With this option, we will share access to all the ongoing batch details for a period of 1 -3 years, so that you can attend live sessions from any batch and learn at your own pace. This option is best for people working in shifts or on weekends. Flexi Subscription Time-line: Foundational Data Science and AI Training course- 1 Year, Advance Data Science and AI Certification course- 3 Years, Data Science and AI Certification Course for Managers and Leaders- 3 Years, Data Science and AI Training with Placement Assistance  - 3-years.',
+            ans: 'Those who enroll for Data Science and Gen AI live classroom training are eligible for a Flexi Pass. With this option, we will share access to all the ongoing batch details for a period of 1 -3 years, so that you can attend live sessions from any batch and learn at your own pace. This option is best for people working in shifts or on weekends. ',
             open: false,
           },
           {
