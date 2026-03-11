cfa568123e1a37aada4d5ef4c6faf41378480939
Sushma <sushmabendre@learnbay.co>
2026-03-11 13:19:31 +0530

    logo added

--- src/pages/datascience/mumbai/business-analytics-course-training-in-mumbai.js
+++ src/pages/datascience/mumbai/business-analytics-course-training-in-mumbai.js
@@ -431,7 +431,7 @@ export default function Home() {
           firstHeading="Business Analytics Course"
           firstTopPara="Industry-paced Project-based Training"
           cityParaCont="The best-suited course for working professionals with less than a year of work experience and dreaming for a lucrative and even growing future."
-          ibmandms={true}
+          iitptnaandms={true}
         />
         <SecondSection
           SecondSectionData={BAMumbaiCourseData[0].secondSection}
