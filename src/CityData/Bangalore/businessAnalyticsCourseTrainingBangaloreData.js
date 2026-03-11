e9d1dc11033bb279f9051ebc64bca1f62b901f94
Sushma <sushmabendre@learnbay.co>
2026-03-11 14:55:53 +0530

    changes added

--- src/CityData/Bangalore/businessAnalyticsCourseTrainingBangaloreData.js
+++ src/CityData/Bangalore/businessAnalyticsCourseTrainingBangaloreData.js
@@ -704,10 +704,13 @@ export const BABangaloreCourseData = [
       'Learnbay Business Analytics course training is powered by IBM certificates',
     CertificationBot1:
       'Learnbay is a pioneer in training Business analytics at the current industry standard and gives out an IBM certificate on course completion. IBM certificates in your resume would make recruiters shortlist your profile with greater interest. Learnbay has created a milestone by partnering with IBM for Business Analytics training in Bangalore.',
-    src33: 'https://d32and0ii3b8oy.cloudfront.net/web/s3_main/genai.webp',
+    src33:
+      'https://d32and0ii3b8oy.cloudfront.net/web/s3_main/learnbayMain/Azure%2BAI%2BFundamentals.png',
+    src44: 'https://d32and0ii3b8oy.cloudfront.net/web/s3_main/genai.webp',
     src: 'https://d32and0ii3b8oy.cloudfront.net/adlearnbay/dsai.webp',
     src22:
       'https://d32and0ii3b8oy.cloudfront.net/web/s3_main/learnbayMain/new-project-cer-s.webp',
+
     ModuleBot1:
       'Learnbay conducts online and offline classes for Business Analytics course training in Bangalore. The Business Analytics course consists of 200 hrs live and uninterrupted classes from Business Analyst experts. Learnbay has batches even on weekends for professionals to learn Business Analytics. Students learning Business analytics get a one-year subscription for completing training at their own comfort and pace of learning. Our faculties encourage students to take initiative in webinars. Learnbay even conducts webinars so that students have fun while they learn.',
     ModuleBot2:
