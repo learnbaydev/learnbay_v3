94fc17cdcfaf95af03a0d7a0a2f625fb8363d677
sohel <syed.sohel@learnbay.co>
2026-04-29 19:12:35 +0530

    added images

--- src/components/CorporateAiTraining/HeroSection/HeroSection.js
+++ src/components/CorporateAiTraining/HeroSection/HeroSection.js
@@ -1,7 +1,17 @@
+import Image from 'next/image';
 import React from 'react';
 
 const HeroSection = () => {
-  return <div>HeroSection</div>;
+  return (
+    <div style={{ paddingTop: '200px' }}>
+      <Image
+        src={`https://d32and0ii3b8oy.cloudfront.net/adlearnbay/corporateAITrainingHero.webp`}
+        alt="corporateAITrainingHero"
+        fill
+        priority
+      />
+    </div>
+  );
 };
 
 export default HeroSection;
