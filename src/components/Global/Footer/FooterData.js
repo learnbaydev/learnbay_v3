dda14f365fc43ba06c51b7526be2fc875e2ff144
sohel <syed.sohel@learnbay.co>
2026-08-31 18:33:19 +0530

    fix & issue

--- src/components/Global/Footer/FooterData.js
+++ src/components/Global/Footer/FooterData.js
@@ -39,7 +39,7 @@ export const CityFooterData = [
           {
             id: 7,
             name: 'Cloud Computing Course in Pune',
-            url: '/cloud&devops/pune/online-cloud-computing-course-pune',
+            url: '/cloud-and-devops/pune/online-cloud-computing-course-pune',
           },
         ],
       },
@@ -193,7 +193,7 @@ export const CityFooterData = [
           {
             id: 7,
             name: 'Cloud Computing Course in Bangalore',
-            url: '/cloud&devops/bangalore/online-cloud-computing-course-bangalore',
+            url: '/cloud-and-devops/bangalore/online-cloud-computing-course-bangalore',
           },
         ],
       },
