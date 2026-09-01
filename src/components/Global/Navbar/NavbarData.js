9dc6aaa3e418e60a6ebebc3a72f8d2c9f5a450e3
sohel <syed.sohel@learnbay.co>
2026-09-01 17:46:42 +0530

    Career Services commented out

--- src/components/Global/Navbar/NavbarData.js
+++ src/components/Global/Navbar/NavbarData.js
@@ -1,18 +1,18 @@
 export const menuItem = [
-  {
-    id: 1,
-    name: "Career Services",
-    url: "#servicePro",
-  },
+  // {
+  //   id: 1,
+  //   name: "Career Services",
+  //   url: "#servicePro",
+  // },
 
   {
     id: 3,
-    name: "Alumni Reviews",
-    url: "/reviews",
+    name: 'Alumni Reviews',
+    url: '/reviews',
   },
   {
     id: 4,
-    name: "AI Co-lab",
-    url: "/ai-co-lab",
+    name: 'AI Co-lab',
+    url: '/ai-co-lab',
   },
 ];
